7f030d7f45cfaee352589d2b6f6fd405183b986c
DavidHul <80717144+DavHul@users.noreply.github.com>
2023-12-07 21:02:41 +0100

    added files for SUT side
    main program and tests for the SUT (subject under test) side

---
+++ SUT_side/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO.Ports;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Collections.Generic; 
+using System.Net.NetworkInformation;
+
+using static tests.Testmodel_sut;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+string wait_for_message(SerialPort serialPort){
+    while (true){
+        try{
+            string message = serialPort.ReadLine();
+            return message;
+        }catch (TimeoutException) { }
+    }
+}
+
+tests.Testmodel_sut test = new tests.Testmodel_sut();
+SerialPort serialPort = test.createSerialPort("/dev/ttyUSB0", 9600);
+test.serialTest();
+string message = wait_for_message(serialPort);
+if (message == "Start ethernet test"){
+    List<bool> ethernet_test = await test.test_ethernet_port();
+    serialPort.WriteLine("Result ethernet test;"+ethernet_test[0].ToString()+";"+ethernet_test[1].ToString());
+}
+
+serialPort.Close();
---
+++ SUT_side/testmodel.cs
@@ -0,0 +1,120 @@
+using System;
+using System.IO.Ports;
+using System.Device.Gpio;
+using System.Threading;
+using System.Linq;
+using System.Net; 
+using System.Net.NetworkInformation;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Drawing;
+using System.Net.Http;
+
+//deze is voor SUT
+namespace tests{
+    class Testmodel_sut{
+        SerialPort serialPort;
+        GpioController controller;
+        HttpClient client;
+
+        public Testmodel_sut(int timeout = 5){
+            client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(timeout);
+        }
+        
+        /// <summary>
+        /// Function for the serial port's creation and setup
+        /// </summary>
+        /// <param name="port">String that refers to the serial port you want to use</param>
+        /// <param name="baudrate">Int for the baudrate</param>
+        public SerialPort createSerialPort(string port, int baudrate){
+            serialPort = new SerialPort(port, baudrate);
+            serialPort.ReadTimeout = 3000;
+            serialPort.WriteTimeout = 2000;
+            serialPort.RtsEnable = true;
+            serialPort.DtrEnable = true;
+            serialPort.Open();
+            return serialPort;
+        }
+
+        /// <summary>
+        /// Function to test the serial connection.
+        /// It performs a handshake with the SUT. If it fails max_attempts times then it returns false. If it succeeds then it returns true
+        /// </summary>
+        public void serialTest(){
+            while (true){
+                try{
+                    string message = serialPort.ReadLine();
+                    Console.WriteLine(message);
+                    if (message == "Hello there"){
+                        serialPort.Write("General Kenobi\n");
+                        message = serialPort.ReadLine();
+                        Console.WriteLine(message);
+                        if (message == "Greeting complete?"){
+                            for (int i = 0; i < 3; i++){ //repeat 3 times because sometimes it doesn't catch the first time
+                                serialPort.Write("Greeting complete!\n");
+                            }
+                            break;
+                        }
+                    }
+                }catch (TimeoutException) { }
+            }
+            
+        }
+
+        /// <summary>
+        /// Private function to check if the device has an IP address
+        /// </summary>
+        /// <returns>Boolean true if the device has an IP address, else it returns false</returns>
+        private bool ip_test(){
+            string hostName = Dns.GetHostName();
+            string myIP = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+            if (myIP != ""){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        /// <summary>
+        /// Function to test the connection to the World Wide Web. It sends a request to an URL and
+        /// if it gets something back then it returns true, else it returns false
+        /// </summary>
+        /// <param name="target_url">String that contains the URL of the webpage you want to check</param>
+        /// <returns>Boolean true if the test succeeded. False if it failed</returns>
+        private async Task<bool> www_test(string target_url){
+            try{
+                // Above three lines can be replaced with new helper method below
+                string responseBody = await client.GetStringAsync(target_url);
+
+                //Console.WriteLine(responseBody);
+                if (responseBody != ""){
+                    return true;
+                }else{
+                    return false;
+                }
+            }catch (HttpRequestException e){
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Function that tests the ethernet port. It uses the private functions ip_test() and www_test()
+        /// 
+        /// </summary>
+        /// <param name="ping_target">String of the URL of the webpage you want the device to be able to reach</param>
+        /// <returns>Returns a list of booleans for each test one. The type is Task<List<bool>> because the function is asynchronous"<</returns>
+        public async Task<List<bool>> test_ethernet_port(string ping_target= "https://google.com"){
+            List<bool> results = new List<bool>();
+            bool ip = ip_test();
+            results.Add(ip);
+            
+            bool ping = await www_test(ping_target);
+            results.Add(ping);
+
+            return results;
+        }
+
+    }
+};
